dfbafa078876d73c6ffdcd7da979f8444cc075f2
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-08-28 12:49:50 +0300

    book-store

---
+++ src/components/constans.js
@@ -0,0 +1,276 @@
+const catalog = [
+    {
+        id: 0,
+        img: "/img/books/232a076f-e14a-11e9-811a-ac1f6b909025-1800x1200.jpg",
+        alt: "Big Book of Science Things to Make and Do",
+        title: "Big Book of Science Things to Make and Do",
+        author: "Leonie Pratt",
+        info: "Big Book of Science. Things to Make and Do | This inspiring activity book introduces children to the world of science in a practical and interactive way. Contains over fifty activities, each using simple and safe materials and accompanied by step-by-step instructions and illustrations. A comprehensive resource for school or home use.",
+        price: "449",
+        priceSumBasket: 449,
+        counterBasket: 1,
+        rating: {
+            num: 3,
+            classRating: 'three'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9781474903271",
+            publishingHouse: "Usborne Publishing Ltd",
+            numberOfPages: '96',
+            yearOfPublication: '2015',
+            format: 'Hardcover'
+
+        }
+    },
+    {
+        id: 1,
+        img: "/img/books/9781602865280_1-1800x1200.jpg",
+        alt: "Harry and Meghan (Revised): Life, Loss, and Love",
+        title: "Harry and Meghan (Revised): Life, Loss, and Love",
+        author: "Katie Nicholl",
+        info: "From his earliest public appearances as a mischievous redheaded toddler, Prince Harry has captured the hearts of royal enthusiasts around the world, while his marriage to Meghan Markle has only endeared him further. In Harry and Meghan, Britain's leading expert on the young royals offers an in-depth look at the wayward prince turned national treasure. Nicholl sheds new light on growing up royal, Harry's relationship with his mother, his troubled youth and early adulthood, and how his military service in Afghanistan inspired him to create his legacy, the Invictus Games. She uncovers new information about Harry's past relationships and reveals the true story of his romance with Meghan Markle, the smart and spirited American who captured his heart. Harry and Meghan: Life, Loss, and Love features interviews with friends, those who have worked with the prince, and former Palace aides. Nicholl reveals behind-the-scenes details about the run up to the couple's spectacular wedding day, their first tours as a married couple, and their preparations for the arrival of the youngest new royal. Harry and Meghan is a compelling portrait of the Royal Family's most popular royal couple and the story of the most gripping royal romance in a decade.",
+        price: "448",
+        priceSumBasket: 448,
+        counterBasket: 1,
+        rating: {
+            num: 3,
+            classRating: 'three'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9781602865280",
+            publishingHouse: "Hachette",
+            numberOfPages: '304',
+            yearOfPublication: '2019',
+            format: 'Paperback'
+
+        }
+    },
+    {
+        id: 2,
+        img: "/img/books/b36eed4d-07bd-11ea-811c-ac1f6b909025-1800x1200.jpg",
+        alt: "The Institute",
+        title: "The Institute",
+        author: "Stephen King",
+        info: "Deep in the woods of Maine, there is a dark state facility where kids, abducted from across the United States, are incarcerated. In the Institute they are subjected to a series of tests and procedures meant to combine their exceptional gifts - telepathy, telekinesis - for concentrated effect.\n" +
+            "Luke Ellis is the latest recruit. He's just a regular 12-year-old, except he's not just smart, he's super-smart. And he has another gift which the Institute wants to use...\n" +
+            "Far away in a small town in South Carolina, former cop Tim Jamieson has taken a job working for the local sheriff. He's basically just walking the beat. But he's about to take on the biggest case of his career.",
+        price: "775",
+        priceSumBasket: 775,
+        counterBasket: 1,
+        rating: {
+            num: 5,
+            classRating: 'five'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9781529355390",
+            publishingHouse: "Hodder & Stoughton",
+            numberOfPages: '496',
+            yearOfPublication: '2019',
+            format: 'Hardcover'
+
+        }
+    },
+    {
+        id: 3,
+        img: "/img/books/9783961711673-1800x1200.jpg",
+        alt: "Porsche Book: The Best Porsche Images by Frank M. Orel",
+        title: "Porsche Book: The Best Porsche Images by Frank M. Orel",
+        author: "Frank M. Orel, Elmar Brümmer",
+        info: "An extended edition of an absolute bestseller: This turbo-charged book is an exciting thrill ride for all lovers of the Porsche experience. As you browse, you embark on a nostalgic, image-packed journey through the annals of high-performance motor engineering and design. The book s dynamic layout and design capture every element of these ultimate rides power, freedom and speed. Captivated from the first glance, you will keep coming back for more! After a while, the book becomes like your own personal road movie as the sights and sounds come to life. Revel in innovative images of all the most famous Porsches shot in glamorous locales across the globe.",
+        price: "2850",
+        priceSumBasket: 2850,
+        counterBasket: 1,
+        rating: {
+            num: 5,
+            classRating: 'five'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9783961711673",
+            publishingHouse: "teNeues",
+            numberOfPages: '368',
+            yearOfPublication: '2018',
+            format: 'Hardcover'
+
+        }
+    },
+    {
+        id: 5,
+        img: "/img/books/9781471190131-1800x1200.jpg",
+        alt: "Too Much and Never Enough: How My Family Created the World's Most Dangerous Man",
+        title: "Too Much and Never Enough: How My Family Created the World's Most Dangerous Man",
+        author: "Mary L. Trump Ph.D.",
+        info: "In this revelatory, authoritative portrait of Donald J. Trump and the toxic family that made him, Mary L. Trump, a trained clinical psychologist and Donald’s only niece, shines a bright light on the dark history of their family in order to explain how her uncle became the man who now threatens the world’s health, economic security and social fabric.\n" +
+            "Mary Trump spent much of her childhood in her grandparents’ large, imposing house in New York, where Donald and his four siblings grew up. She describes a nightmare of traumas, destructive relationships and a tragic combination of neglect and abuse. She explains how specific events and general family patterns created the damaged man who currently occupies the Oval Office, including the strange and harmful relationship between Fred Trump and his two oldest sons, Fred Jr. and Donald.",
+        price: "954",
+        priceSumBasket: 954,
+        counterBasket: 1,
+        rating: {
+            num: 4,
+            classRating: 'four'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9781471190131",
+            publishingHouse: "Simon & Schuster",
+            numberOfPages: '240',
+            yearOfPublication: '2020',
+            format: 'Hardcover'
+
+        }
+    },
+    {
+        id: 6,
+        img: "/img/books/8f304844-07b9-11ea-811c-ac1f6b909025-1800x1200.jpg",
+        alt: "The Testaments",
+        title: "The Testaments",
+        author: "Margaret Atwood",
+        info: "More than fifteen years after the events of The Handmaid’s Tale, the theocratic regime of the Republic of Gilead maintains its grip on power, but there are signs it is beginning to rot from within. At this crucial moment, the lives of three radically different women converge, with potentially explosive results.\n" +
+            "Two have grown up as part of the first generation to come of age in the new order. The testimonies of these two young women are joined by a third voice: a woman who wields power through the ruthless accumulation and deployment of secrets.",
+        price: "775",
+        priceSumBasket: 775,
+        counterBasket: 1,
+        rating: {
+            num: 4,
+            classRating: 'four'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9781784742324",
+            publishingHouse: "Chatto & Windus",
+            numberOfPages: '432',
+            yearOfPublication: '2019',
+            format: 'Hardcover'
+
+        }
+    },
+    {
+        id: 7,
+        img: "/img/books/81750ee9-0787-11ea-811c-ac1f6b909025-1800x1200.jpg",
+        alt: "Shoe Dog: A Memoir by the Creator of NIKE",
+        title: "Shoe Dog: A Memoir by the Creator of NIKE",
+        author: "Phil Knight",
+        info: "Shoe Dog: A Memoir by the Creator of NIKE | In 1962, fresh out of business school, Phil Knight borrowed $50 from his father and created a company with a simple mission: import high-quality, low-cost athletic shoes from Japan. Selling the shoes from the boot of his Plymouth, Knight grossed $8000 in his first year. Today, Nike's annual sales top $30 billion. In an age of start-ups, Nike is the ne plus ultra of all start-ups, and the swoosh has become a revolutionary, globe-spanning icon, one of the most ubiquitous and recognisable symbols in the world today.\n" +
+            "But Knight, the man behind the swoosh, has always remained a mystery. Now, for the first time, he tells his story. Candid, humble, wry and gutsy, he begins with his crossroads moment when at 24 he decided to start his own business. He details the many risks and daunting setbacks that stood between him and his dream - along with his early triumphs. Above all, he recalls how his first band of partners and employees soon became a tight-knit band of brothers. Together, harnessing the transcendent power of a shared mission, and a deep belief in the spirit of sport, they built a brand that changed everything.",
+        price: "320",
+        priceSumBasket: 320,
+        counterBasket: 1,
+        rating: {
+            num: 4,
+            classRating: 'four'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9781471146725",
+            publishingHouse: "Simon & Schuster",
+            numberOfPages: '400',
+            yearOfPublication: '2018',
+            format: 'Paperback'
+
+        }
+    },
+    {
+        id: 8,
+        img: "/img/books/9780008234188_1-1800x1200.jpg",
+        alt: "The Woman in the Window",
+        title: "The Woman in the Window",
+        author: "A. J. Finn",
+        info: "It’s been ten months since Anna Fox last left her home. Ten months during which she has haunted the rooms of her old New York house, lost in her memories, too terrified to step outside.Anna’s lifeline to the real world is her window, where she sits, watching her neighbours. When the Russells move in, Anna is instantly drawn to them. A picture-perfect family, they are an echo of the life that was once hers.But one evening, a scream rips across the silence, and Anna witnesses something horrifying. Now she must uncover the truth about what really happened. But if she does, will anyone believe her? And can she even trust herself?",
+        price: "288",
+        priceSumBasket: 288,
+        counterBasket: 1,
+        rating: {
+            num: 3,
+            classRating: 'three'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9781471146725",
+            publishingHouse: "Harper Collins",
+            numberOfPages: '448',
+            yearOfPublication: '2018',
+            format: 'Paperback'
+
+        }
+    },
+    {
+        id: 9,
+        img: "/img/books/9780241434642-1800x1200.jpg",
+        alt: "Artemis Fowl and the Arctic Incident. Book 2",
+        title: "Artemis Fowl and the Arctic Incident. Book 2",
+        author: "Eoin Colfer",
+        info: "Goblin gangs (exactly as bad as it sounds) are planning an uprising, and it looks like they've had human help.\n" +
+            "Holly Short is convinced it's her old nemesis, Artemis Fowl.\n" +
+            "But, for once, Artemis is innocent. He's too busy getting his father back from the Arctic Circle, where the Russian Mafia (also very bad) is holding him prisoner.\n" +
+            "All Artemis has to do is clear his name, face the biggest criminal organisations in the world and avoid freezing to death.",
+        price: "225",
+        priceSumBasket: 225,
+        counterBasket: 1,
+        rating: {
+            num: 2,
+            classRating: 'two'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9780241434642",
+            publishingHouse: "Puffin",
+            numberOfPages: '336',
+            yearOfPublication: '2006',
+            format: 'Paperback'
+
+        }
+    },
+    {
+        id: 10,
+        img: "/img/books/5133cfb6-e518-11e9-811a-ac1f6b909025-1800x1200.jpg",
+        alt: "Solutions 2nd Edition Intermediate: Student's Book",
+        title: "Solutions 2nd Edition Intermediate: Student's Book",
+        author: "Paul A Davies Tim Falla",
+        info: "One lesson-per-page structure with achievable goals, clear signposting of the skill being practised, and gradual build-up to a productive outcome, gives everyone a sense of achievement after every class Updated, motivating texts with effective vocabulary exploitation activities for increased engagement and active practice Speaking is integrated into all lessons, with step-by-step guidance leading to real-life practice, to make the activities meaningful and memorable Dedicated writing page in every unit provides model texts and a step by step guide for producing their own piece of work Get Ready for Your Exam pages, Productive tasks, 'Check your work' sections, and Exam Strategy boxes, ensure students are well prepared for their school leaving exam .",
+        price: "293",
+        priceSumBasket: 293,
+        counterBasket: 1,
+        rating: {
+            num: 5,
+            classRating: 'five'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9780194552882",
+            publishingHouse: "Oxford University Press",
+            numberOfPages: '136',
+            yearOfPublication: '2012',
+            format: 'Paperback'
+
+        }
+    },
+    {
+        id: 11,
+        img: "/img/books/9780723270874-1800x1200.jpg",
+        alt: "Little Women (Ladybird Classics)",
+        title: "Little Women (Ladybird Classics)",
+        author: "Louisa May Alcott",
+        info: "This Ladybird Classic is an abridged retelling of the classic story of Little Women by Louisa May Alcott, making it perfect for introducing the story to younger children, or for newly confident readers to tackle alone.\n" +
+            "Beautiful new illustrations throughout and a gorgeous larger format with ribbon marker bring the magic of this classic story to a new generation of children.",
+        price: "192",
+        priceSumBasket: 192,
+        counterBasket: 1,
+        rating: {
+            num: 3,
+            classRating: 'three'
+        },
+        buy: "Put in basket",
+        detailedInformation: {
+            ISBN: "9780723270874",
+            publishingHouse: "Ladybird",
+            numberOfPages: '72',
+            yearOfPublication: '2014',
+            format: 'Hardcover'
+
+        }
+    }]
+
+export default catalog;
--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import catalog from "../constans";
 import './Content.css';
 
 const Content = () => (
     <div className="content">
-        content
+        <div className="wrap-books">
+            {catalog.map((item) =>
+                <div className="book-item" key={item.id}>
+                    <div className="img-book">
+                        <img src={item.img} alt={item.alt}/>
+                    </div>
+                    <div className="title-book">
+                        <h4>{item.title}</h4>
+                        <h5>{item.author}</h5>
+                    </div>
+                    <div className="btn-book">
+                        <button>{item.buy}</button>
+                    </div>
+                    <div className="price-book">uah {item.price}</div>
+                    <div className="rating-book">
+                        Rating:
+                        <div className="rating-star">
+                            <div className={`${"rating-span"} ${item.rating.classRating}`}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                            <div className="quantityStar">{item.rating.num}/5</div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     </div>
 )
 
