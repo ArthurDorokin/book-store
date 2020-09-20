a094f284ce62ba3690a6bbc03ab2f56e054c5429
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-09-20 23:26:12 +0300

    book-store

--- src/components/redux/reducers/details.js
+++ src/components/redux/reducers/details.js
@@ -5,14 +5,13 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-    console.log('reducers details - ', initialState);
     switch (action.type) {
         case "TAKE_PRODUCT":
+            console.log({selectProduct: books.filter(item => item.id === action.payload)})
              return {
                  ...state,
                  selectProduct: books.filter(item => item.id === action.payload)
              }
-        //console.log({selectProduct: books.filter(item => item.id === action.payload)})
         default:
             return state;
     }
